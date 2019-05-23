b3e759127d859c90e8e21700a9a5714a3b828f0a
JC Brand <jc@opkode.com>
2019-05-23 13:33:32 +0200

    ES6 updates
    - Use ES6 features instead of underscore methods
    - Update to ES6 imports and modules
    - Extend with lodash methods instead of underscore, so that we can import individual methods
    - Make Backbone and lodash peer dependencies

--- CHANGES.md
+++ CHANGES.md
@@ -1,6 +1,11 @@
 Changelog
 =========
 
+1.0.3 (Unreleased)
+------------------
+
+* Remove all views on `reset` event
+
 1.0.2 (2018-01-29)
 ------------------
 
--- backbone.overview.js
+++ backbone.overview.js
@@ -1,164 +1,166 @@
 /*!
- * Backbone.Overview 
+ * Backbone.Overview
  *
- * Copyright (c) 2018, JC Brand <jc@opkode.com>
- * Licensed under the Mozilla Public License (MPL) 
+ * Copyright (c) JC Brand <jc@opkode.com>
+ * Licensed under the Mozilla Public License (MPL)
  */
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(["underscore", "backbone"], factory);
-   } else {
-      // RequireJS isn't being used.
-      // Assume underscore and backbone are loaded in <script> tags
-        factory(_ || root._, Backbone || root.Backbone);
+import  {
+   chain, includes, debounce, detect,
+   difference, drop, each,
+   every, extend, filter,
+   find, first, forEach, get,
+   head, indexOf,
+   initial, invoke,
+   isEmpty, last, lastIndexOf,
+   map, max, min,
+   reduce, reduceRight,
+   reject, rest, sample,
+   shuffle, size,
+   some, sortBy, tail,
+   take, toArray, without
+} from 'lodash';
+
+
+const View = Backbone.NativeView === undefined ? Backbone.View : Backbone.NativeView;
+
+
+const Overview = Backbone.Overview = function (options) {
+   /* An Overview is a View that contains and keeps track of sub-views.
+   * Kind of like what a Collection is to a Model.
+   */
+   this.views = {};
+   this.keys = () => Object.keys(this.views);
+   this.getAll = () => this.views;
+   this.get = id => this.views[id];
+
+   /* Exclusive get. Returns all instances except the given id. */
+   this.xget = id => {
+      return this.keys()
+         .filter(k => (k !== id))
+         .reduce((acc, k) => {
+            acc[k] = this.views[k]
+            return acc;
+         }, {});
    }
-}(this, function (_, Backbone) {
-    "use strict";
 
-    const View = _.isUndefined(Backbone.NativeView) ? Backbone.View : Backbone.NativeView;
+   this.add = (id, view) => {
+      this.views[id] = view;
+      return view;
+   };
 
-    const Overview = Backbone.Overview = function (options) {
-        /* An Overview is a View that contains and keeps track of sub-views.
-         * Kind of like what a Collection is to a Model.
-         */
-        var that = this;
-        this.views = {};
-        this.keys = _.partial(_.keys, this.views);
-        this.getAll = _.partial(_.identity, this.views);
-
-        this.get = function (id) {
-            return that.views[id];
-        };
-
-        this.xget = function (id) {
-            /* Exclusive get. Returns all instances except the given id. */
-            return _.filter(that.views, function (view, vid) {
-                return vid !== id;
-            });
-        };
-
-        this.add = function (id, view) {
-            that.views[id] = view;
+   this.remove = id => {
+      if (typeof id === "undefined") {
+            new View().remove.apply(this);
+      }
+      const view = this.views[id];
+      if (view) {
+            delete this.views[id];
+            view.remove();
             return view;
-        };
+      }
+   };
 
-        this.remove = function (id) {
-            if (typeof id === "undefined") {
-                new View().remove.apply(that);
-            }
-            var view = that.views[id];
-            if (view) {
-                delete that.views[id];
-                view.remove();
-                return view;
-            }
-        };
+   this.removeAll = () => {
+      this.keys().forEach(id => this.remove(id));
+      return this;
+   }
 
-        this.removeAll = function () {
-            _.each(_.keys(that.views), that.remove);
-            return that;
-        };
-        View.apply(this, Array.prototype.slice.apply(arguments));
-    };
-
-    var methods = [
-        'all', 'any', 'chain', 'collect', 'contains', 'detect',
-        'difference', 'drop', 'each', 'every', 'filter', 'find',
-        'first', 'foldl', 'foldr', 'forEach', 'head', 'include',
-        'indexOf', 'initial', 'inject', 'invoke', 'isEmpty',
-        'last', 'lastIndexOf', 'map', 'max', 'min', 'reduce',
-        'reduceRight', 'reject', 'rest', 'sample', 'select',
-        'shuffle', 'size', 'some', 'sortBy', 'tail', 'take',
-        'toArray', 'without',
-    ];
-    // Mix in each Underscore method as a proxy to `Overview#view`.
-    _.each(methods, function(method) {
-        Overview.prototype[method] = function() {
-            var args = Array.prototype.slice.call(arguments);
-            args.unshift(this.views);
-            return _[method].apply(_, args);
-        };
-    });
-    _.extend(Overview.prototype, View.prototype);
-    Overview.extend = View.extend;
+   View.apply(this, Array.prototype.slice.apply(arguments));
+};
 
 
-    Backbone.OrderedListView = Backbone.Overview.extend({
+const methods = {
+   chain, includes, difference, drop,
+   every, filter, find,
+   first, forEach, head,
+   indexOf, initial, invoke, isEmpty,
+   last, lastIndexOf, map, max, min, reduce,
+   reduceRight, reject, rest, sample,
+   shuffle, size, some, sortBy, tail, take,
+   toArray, without
+}
+Object.keys(methods).forEach(name => {
+   Overview.prototype[name] = function() {
+      const args = Array.prototype.slice.call(arguments);
+      args.unshift(this.views);
+      return methods[name].apply(this, args);
+   };
+});
 
-        // The `listItems` attribute denotes the path (from this View) to the
-        // list of items.
-        listItems: 'model',
-        // The `sortEvent` attribute specifies the event which should cause the
-        // ordered list to be sorted.
-        sortEvent: 'change',
-        // The `listSelector` is the selector used to query for the DOM list
-        // element which contains the ordered items.
-        listSelector: '.ordered-items',
-        // The `itemView` is constructor which should be called to create a
-        // View for a new item.
-        ItemView: undefined,
+extend(Overview.prototype, View.prototype);
+Overview.extend = View.extend;
 
-        initialize () {
-            this.sortEventually = _.debounce(this.sortAndPositionAllItems.bind(this), 500);
-            this.items = _.get(this, this.listItems);
-            this.items.on('add', this.createItemView, this);
-            this.items.on('add', this.sortEventually, this);
+
+const OrderedListView = Backbone.OrderedListView = Backbone.Overview.extend({
+   /* An OrderedListView is a special type of Overview which adds some
+    * methods and conventions for rendering an ordered list of elements.
+    */
+   // The `listItems` attribute denotes the path (from this View) to the
+   // list of items.
+   listItems: 'model',
+   // The `sortEvent` attribute specifies the event which should cause the
+   // ordered list to be sorted.
+   sortEvent: 'change',
+   // The `listSelector` is the selector used to query for the DOM list
+   // element which contains the ordered items.
+   listSelector: '.ordered-items',
+   // The `itemView` is constructor which should be called to create a
+   // View for a new item.
+   ItemView: undefined,
+   // The `subviewIndex` is the attribute of the list element model which
+   // acts as the index of the subview in the overview.
+   // An overview is a "Collection" of views, and they can be retrieved
+   // via an index. By default this is the 'id' attribute, but it could be
+   // set to something else.
+   subviewIndex: 'id',
+
+   initialize () {
+      this.sortEventually = debounce(this.sortAndPositionAllItems.bind(this), 250);
+
+      this.items = get(this, this.listItems);
+      this.items.on('add', this.sortAndPositionAllItems, this);
+      this.items.on('remove', this.removeView, this);
+      this.items.on('reset', this.removeAll, this);
+      if (this.sortEvent) {
             this.items.on(this.sortEvent, this.sortEventually, this);
-        },
+      }
+   },
 
-        createItemView (item) {
-            let item_view = this.get(item.get('id'));
-            if (!item_view) {
-                item_view = new this.ItemView({model: item});
-                this.add(item.get('id'), item_view);
-            } else {
-                item_view.model = item;
-                item_view.initialize();
+   createItemView (item) {
+      let item_view = this.get(item.get(this.subviewIndex));
+      if (!item_view) {
+            item_view = new this.ItemView({model: item});
+            this.add(item.get(this.subviewIndex), item_view);
+      } else {
+            item_view.model = item;
+            item_view.initialize();
+      }
+      item_view.render();
+      return item_view;
+   },
+
+   removeView (item) {
+      this.remove(item.get(this.subviewIndex));
+   },
+
+   sortAndPositionAllItems () {
+      if (!this.items.length) {
+            return;
+      }
+      this.items.sort();
+
+      const list_el = this.el.querySelector(this.listSelector);
+      const div = document.createElement('div');
+      list_el.parentNode.replaceChild(div, list_el);
+      this.items.forEach(item => {
+            let view = this.get(item.get(this.subviewIndex));
+            if (!view) {
+               view = this.createItemView(item)
             }
-            item_view.render();
-            return item_view;
-        },
+            list_el.insertAdjacentElement('beforeend', view.el);
+      });
+      div.parentNode.replaceChild(list_el, div);
+   }
+});
 
-
-        sortAndPositionAllItems () {
-            this.items.sort();
-            this.items.each((item) => {
-                if (_.isUndefined(this.get(item.get('id')))) {
-                    this.createItemView(item)
-                }
-                this.positionItem(item, this.el.querySelector(this.listSelector));
-            });
-        },
-
-        positionItem (item, list_el) {
-            /* Place the View's DOM element in the correct alphabetical
-             * position in the list.
-             *
-             * IMPORTANT: there's an important implicit assumption being
-             * made here. And that is that initially this method gets called
-             * for each item in the right positional order.
-             *
-             * In other words, it gets called for the 0th, then the
-             * 1st, then the 2nd, 3rd and so on.
-             *
-             * That's why we call it in the "success" handler after
-             * fetching the items, so that we know we have ALL of
-             * them and that they're sorted.
-             */
-            const view = this.get(item.get('id')),
-                index = this.items.indexOf(item);
-
-            if (index === 0) {
-                list_el.insertAdjacentElement('afterbegin', view.el);
-            } else if (index === (this.items.length-1)) {
-                list_el.insertAdjacentElement('beforeend', view.el);
-            } else {
-                const neighbour_el = list_el.querySelector('li:nth-child('+index+')');
-                neighbour_el.insertAdjacentElement('afterend', view.el);
-            }
-            return view;
-        }
-    });
-
-    return Backbone.Overview;
-}));
+export { OrderedListView, Overview }
